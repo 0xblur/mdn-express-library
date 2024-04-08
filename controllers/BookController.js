6d814380b5874a2ba18265178606ea0fc45a6508
cypherHades <xjadezbix@gmx.com>
2024-04-08 09:49:54 -0500

    refactor: syntax

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -6,32 +6,30 @@ import BookInstance from "../db/models/BookInstance.js";
 import asyncHandler from "express-async-handler";
 
 export default class BookController {
-	static index() {
-		asyncHandler(async (req, res, next) => {
-			const [
-				numBooks,
-				numBookInstances,
-				numAvailableBookInstances,
-				numAuthors,
-				numGenres,
-			] = await Promise.all([
-				Book.countDocuments({}).exec(),
-				BookInstance.countDocuments({}).exec(),
-				BookInstance.countDocuments({ status: "Available" }).exec(),
-				Author.countDocuments({}).exec(),
-				Genre.countDocuments({}).exec(),
-			]);
+	static index = asyncHandler(async (req, res, next) => {
+		const [
+			numBooks,
+			numBookInstances,
+			numAvailableBookInstances,
+			numAuthors,
+			numGenres,
+		] = await Promise.all([
+			Book.countDocuments({}).exec(),
+			BookInstance.countDocuments({}).exec(),
+			BookInstance.countDocuments({ status: "Available" }).exec(),
+			Author.countDocuments({}).exec(),
+			Genre.countDocuments({}).exec(),
+		]);
 
-			res.render("index", {
-				title: "Local Library Home",
-				book_count: numBooks,
-				book_instance_count: numBookInstances,
-				book_instance_available_count: numAvailableBookInstances,
-				author_count: numAuthors,
-				genre_count: numGenres,
-			});
+		res.render("index", {
+			title: "Local Library Home",
+			book_count: numBooks,
+			book_instance_count: numBookInstances,
+			book_instance_available_count: numAvailableBookInstances,
+			author_count: numAuthors,
+			genre_count: numGenres,
 		});
-	}
+	});
 
 	// Display list of all books.
 	static bookList = asyncHandler(async (req, res, next) => {
