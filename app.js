741a07a183a46eabd1c715ad52f45e0431281cd0
cypherHades <xjadezbix@gmx.com>
2024-04-11 11:04:36 -0500

    mod(handlebars): add more helpers from library

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 // view engine setup
 const __dirname = "./";
 const viewsDir = path.join(__dirname, "views");
-const helpers = hbsHelpers("comparison");
+const helpers = hbsHelpers(["comparison", "array", "math", "misc"]);
 const hbs = create({
 	extname: ".hbs",
 	handlebars: allowInsecurePrototypeAccess(Handlebars),
