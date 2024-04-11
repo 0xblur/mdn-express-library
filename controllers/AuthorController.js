0c654520b7d295f9382d0d07f8ad05a9ef7fdcd2
cypherHades <xjadezbix@gmx.com>
2024-04-11 10:18:24 -0500

    mod(controllers): change `AuthorController` static functions to instance methods

--- controllers/AuthorController.js
+++ controllers/AuthorController.js
@@ -3,46 +3,39 @@ import asyncHandler from "express-async-handler";
 import { connectToDB } from "../db/utils.js";
 
 export default class AuthorController {
-	static authorList = asyncHandler(async (req, res, next) => {
-		res.send("NOT YET IMPLEMENTED: Author list");
-	});
 	constructor() {
 		AuthorController.init();
 	}
 
-	static authorDetail = asyncHandler(async (req, res, next) => {
-		res.send(`NOT YET IMPLEMENTED: Author detail: ${req.params.id}`);
 	static async init() {
 		await connectToDB();
 	}
 
 	});
 
-	static authorCreateGet = asyncHandler(async (req, res, next) => {
-		res.send("NOT YET IMPLEMENTED: Author create GET");
 	});
 
-	static authorCreatePost = asyncHandler(async (req, res, next) => {
-		res.send("NOT YET IMPLEMENTED: Author create POST");
+	authorCreateGet = asyncHandler(async (req, res, next) => {
+		res.render("author_form", { title: "Create Author" });
 	});
 
 	// Display author-delete form on GET.
-	static authorDeleteGet = asyncHandler(async (req, res, next) => {
+	authorDeleteGet = asyncHandler(async (req, res, next) => {
 		res.send("NOT YET IMPLEMENTED: Author delete GET");
 	});
 
 	// Handle Author delete on POST.
-	static authorDeletePost = asyncHandler(async (req, res, next) => {
+	authorDeletePost = asyncHandler(async (req, res, next) => {
 		res.send("NOT YET IMPLEMENTED: Author delete POST");
 	});
 
 	// Display Author update form on GET.
-	static authorUpdateGet = asyncHandler(async (req, res, next) => {
+	authorUpdateGet = asyncHandler(async (req, res, next) => {
 		res.send("NOT YET IMPLEMENTED: Author update GET");
 	});
 
 	// Handle Author update on POST
-	static authorUpdatePost = asyncHandler(async (req, res, next) => {
+	authorUpdatePost = asyncHandler(async (req, res, next) => {
 		res.send("NOT YET IMPLEMENTED: Author update POST");
 	});
 }
